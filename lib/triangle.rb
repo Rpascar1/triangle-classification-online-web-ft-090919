41e743ace0d8ec6148634661ac5ac72b02f66aea
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-26 01:22:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,6 +2,7 @@ class Triangle
  
   def initialize(a,b,c)
     @a=a, @b=b, @c=c
+    
   end
     
    def kind
@@ -11,22 +12,17 @@ class Triangle
     elsif  
       @a == @b || @b == @c ||@a == @c
       :isosolese
-     elsif
-      @
+     else
       :scalene
      end
      
    end
      
-     
-     
-     
-   end
-   
+    
 
  
   class TriangleError < StandardError
-    raise TriangleError if 
+    if 
   
   end
 end
